324d3a2140705ffd22e0f21801d222a8f984e351
Tristan Whitehouse <tsw54@cornell.edu>
2022-12-14 19:55:07 -0500

    commit tree f839a7ef2ecc3f5b6314c08e4a20de2eb1f86be5

---
+++ index.js
@@ -0,0 +1,22 @@
+function introduction(name) {
+
+    return `Hi, my name is ${name}.`;
+
+}
+
+function introductionWithLanguage(name, language) {
+
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+}
+
+function introductionWithLanguageOptional(name, language) {
+
+    if (!language) {
+        
+        language = 'JavaScript';
+
+    }
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+}
